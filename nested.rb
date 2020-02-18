95498fe7a356797d1d422635af38a622bc364559
Frank  D'Angelo <08fdange@gmail.com>
2020-02-18 22:18:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,4 +127,6 @@ def adding_to_dennis
      }
   programmer_hash[:dennis_ritchie][:languages].push("Assembly")
   return programmer_hash
+  #can also be done by using the specific index of the array 
+  #like this: programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
 end
